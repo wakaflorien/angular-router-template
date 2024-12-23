7d5761b7a2081702072f01989df98442edb3cdb6
Florien Niyongabo <waka@192.168.1.71>
2024-12-23 16:26:16 +0200

    Ignore vscode extensions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
