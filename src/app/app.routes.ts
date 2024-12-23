24122db49403355994e06332dc9b5420de313b82
Florien Niyongabo <waka@192.168.1.71>
2024-12-23 16:19:22 +0200

    Angular single page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/heroes-list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'crisis-list',
+    title: 'Crisis List',
+    component: CrisisListComponent,
+  },
+  {
+    path: 'heroes-list',
+    title: 'Heroes List',
+    component: HeroesListComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
